ef1da09d98aa2a14310fc67fb0a8a7e3a1e227b2
Popmiiz Yosprayoon <popmiizpop@popmiiz.local>
2017-10-24 15:49:58 +0700

    Initial Commit

---
+++ Qrproject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Qrproject
+//
+//  Created by popmiiz on 10/24/2560 BE.
+//  Copyright © 2560 popmiiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
